af0116f034c31e789f7fd48786a7432a1ea8fca7
username-sam <218394446+username-sam@users.noreply.github.com>
2025-08-31 01:49:36 +0600

    decimal point isn't working

---
+++ script.js
@@ -0,0 +1,79 @@
+let input = document.querySelector("input");
+let nodeOfButtons = document.querySelectorAll('button');
+let value = '';
+let op;
+let functionalOperator = ["DEL","C","="];
+let mathamaticalOperator = ["+","-","*","/","^","."]
+let buttons = Array.from(nodeOfButtons);
+
+buttons.forEach((button)=>{
+    button.addEventListener("click",(e)=>{
+        if(e.target.textContent == "C"){
+            value = ''
+            input.value = value;
+
+        }else if(e.target.textContent == "DEL"){
+            value = value.substring(0,value.length-1);
+            input.value = value;
+
+        }else if(mathamaticalOperator.includes(e.target.id) && value == ""){
+              value = '';
+              input.value = value;
+
+        }else if(mathamaticalOperator.includes(e.target.id) && mathamaticalOperator.includes(value.charAt(value.length-1))){
+               value = value.substring(0,value.length-1);
+               value += e.target.id;
+               input.value = value;
+               op = e.target.id;
+
+        }else if(mathamaticalOperator.includes(e.target.id) && value !== ""){
+              value += e.target.id;
+              op = e.target.id;
+              input.value = value;
+
+        }else if(e.target.id == "="){
+
+               if(value == ""){
+                input.value = value;
+               }else if(value.charAt(value.length-1) == op){
+                input.value = value;
+               }else{
+                input.value = calculator.calculate(value)
+               }
+
+        }else if(!functionalOperator.includes(e.target.textContent)){
+            value +=  e.target.id
+              input.value = value;
+        }
+    })
+})
+
+
+function Calculator(){
+    this.methods = {
+        "-" : (a,b)=>a-b,
+        "+" : (a,b)=>a+b,
+        "/" : (a,b)=>a/b,
+        "*" : (a,b)=>a*b,
+        "^" : (a,b)=>Math.pow(a,b)
+    };
+
+    this.calculate = function(str){
+        let split =  str.split(op),
+        a = +split[0],
+        b = +split[1]
+        
+     if(isNaN(a) || isNaN(b)){
+        return NaN;
+     }
+     return this.methods[op](a,b)
+    };
+
+    this.addMethods = function(name,func){
+        this.methods[name] = func;
+    }
+}
+
+let calculator = new Calculator;
+
+console.log(calculator.calculate("12+13"));
