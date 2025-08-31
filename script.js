d1009e21887914b2f1ea2de57df1483dd99b9094
username-sam <218394446+username-sam@users.noreply.github.com>
2025-09-01 01:34:19 +0600

    done

--- script.js
+++ script.js
@@ -1,96 +1,3 @@
-let input = document.querySelector("input");
-let nodeOfButtons = document.querySelectorAll('button');
-let value = '';
-let op;
-let functionalOperator = ["DEL","C","="];
-let mathamaticalOperator = ["+","-","*","/","^"]
-let buttons = Array.from(nodeOfButtons);
-
-buttons.forEach((button)=>{
-    button.addEventListener("click",(e)=>{
-        if(e.target.textContent == "C"){
-            value = ''
-            input.value = value;
-
-        }else if(e.target.textContent == "DEL"){
-            value = value.substring(0,value.length-1);
-            input.value = value;
-
-        }else if(mathamaticalOperator.includes(e.target.id)){
-            if(value == ""){
-                value = '';
-                input.value = value;
-
-            }else if(mathamaticalOperator.includes(value.charAt(value.length-1))){
-               value = value.substring(0,value.length-1);
-               value += e.target.id;
-               input.value = value;
-               op = e.target.id;
-
-            }else if(op && !mathamaticalOperator.includes(value.charAt(value.length-1))){
-                value = `${calculator.calculate(value)}`;
-                if(value.includes(".")){
-                    value = (+value).toFixed(1)    
-                }
-                value += e.target.id;
-                input.value = value;
-                op = null
-
-            }else if(!mathamaticalOperator.includes(value.charAt(value.length-1))){
-              value += e.target.id;
-              input.value = value;
-              op = e.target.id;
-              
-            }
-
-        }else if(e.target.id == "." ){
-            if(value.includes(".")){
-                if(op){
-                  let splitedValue = value.split(op)  
-                  if(splitedValue[1] == "" || !splitedValue[1].includes(".")){
-                    value += e.target.id;
-                    input.value = value;
-                  }
-                }else{
-                    input.value = value;    
-                }
-                }else if(value == ""){
-                 value = e.target.id
-                 input.value = value;
-            }else if(value.charAt(value.length-1) == "."){
-                value = value.substring(0,value.length-1);
-                value += e.target.id;
-                input.value = value;
-            }else if (value !== ""){
-                 value += e.target.id;
-                 input.value = value;
-            }
-
-        }else if(e.target.id == "="){
-
-               if(value == ""){
-                input.value = value;
-               }else if(value.charAt(value.length-1) == op){
-                input.value = value;
-               }else if(!op){
-                input.value = value;
-               }else{
-                value = `${calculator.calculate(value)}`
-                if(value.includes(".")){
-                    value = (+value).toFixed(1)    
-                }
-                input.value = value;
-                op = null;
-               }
-
-        }else if(!functionalOperator.includes(e.target.textContent)){
-              value +=  e.target.id
-              input.value = value;
-        }
-    })
-})
-
-
 function Calculator(){
     this.methods = {
         "-" : (a,b)=>a-b,
@@ -114,7 +21,87 @@ function Calculator(){
     this.addMethods = function(name,func){
         this.methods[name] = func;
     }
-}
+};
 
-let calculator = new Calculator;
+let op,
+    input = document.querySelector("input"),
+    nodeOfButtons = document.querySelectorAll('button'),
+    value = '',
+    functionalOperator = ["DEL","C","="],
+    mathamaticalOperator = ["+","-","*","/","^"],
+    buttons = Array.from(nodeOfButtons),
+    calculator = new Calculator;
 
+buttons.forEach((button)=>{
+    button.addEventListener("click",(e)=>{
+        if(e.target.textContent == "C"){
+             value = ''
+             input.value = value;
+        }else if(e.target.textContent == "DEL"){
+             value = value.substring(0,value.length-1);
+             input.value = value;
+             op = null;
+        }else if(mathamaticalOperator.includes(e.target.id)){
+            if(value == ""){
+             value = '';
+             input.value = value;
+            }else if(mathamaticalOperator.includes(value.charAt(value.length-1))){
+             value = value.substring(0,value.length-1);
+             value += e.target.id;
+             input.value = value;
+             op = e.target.id;
+            }else if(op && !mathamaticalOperator.includes(value.charAt(value.length-1))){
+             value = `${calculator.calculate(value)}`;
+            if(value.includes(".")){
+             value = (+value).toFixed(1) 
+            }
+             value += e.target.id;
+             input.value = value;
+             op = null
+            }else if(!mathamaticalOperator.includes(value.charAt(value.length-1))){
+             value += e.target.id;
+             input.value = value;
+             op = e.target.id;
+            }
+        }else if(e.target.id == "." ){
+            if(value.includes(".")){
+            if(op){
+             let splitedValue = value.split(op);
+             if(splitedValue[1] == "" || !splitedValue[1].includes(".")){
+             value += e.target.id;
+             input.value = value;
+             }
+            }else{
+             input.value = value;    
+            }
+            }else if(value == ""){
+             value = e.target.id
+             input.value = value;
+            }else if(value.charAt(value.length-1) == "."){
+             value = value.substring(0,value.length-1);
+             value += e.target.id;
+             input.value = value;
+            }else if (value !== ""){
+             value += e.target.id;
+             input.value = value;
+            }
+        }else if(e.target.id == "="){
+            if(value == ""){
+             input.value = value;
+            }else if(value.charAt(value.length-1) == op){
+             input.value = value;
+            }else if(!op){
+             input.value = value;
+            }else{
+             value = `${calculator.calculate(value)}`
+             if(value.includes(".")){
+             value = (+value).toFixed(1)    
+            }
+             input.value = value;
+             op = null;
+            }
+        }else if(!functionalOperator.includes(e.target.textContent)){
+             value +=  e.target.id
+             input.value = value;
+        }
+    })});
